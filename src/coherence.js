4502cdcc8d15969016ca0ae06a99eec474555cd2
Carlos Lalimarmo <clalimarmo@gmail.com>
2015-11-20 00:55:21 -0500

    Remove Bindings
    these have been replaced with the model.ReactBindings factory

--- src/coherence.js
+++ src/coherence.js
@@ -4,7 +4,6 @@ const Coherence = {};
 
 Coherence.Intent = require('./intent');
 Coherence.Model = require('./model');
-Coherence.Bindings = require('./bindings');
 Coherence.LocationFactory = require('./location-factory');
 
 module.exports = Coherence;
